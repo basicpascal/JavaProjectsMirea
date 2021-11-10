c8ce5bf712ef24ab2c1c06289e326a3fed80e19b
Danila Kalugin <danilakalugin@yandex.ru>
2021-11-10 17:09:31 +0300

    Fixed and completed Task6

--- Task6/src/Student.java
+++ Task6/src/Student.java
@@ -1,13 +1,44 @@
-public class Student {
-    int id;
+package javafoundation.section_6;
 
-    public Student(int id){
-        this.id = id;
+public class Student implements Comparable {
+
+    private int idNum;
+    private int GPA;
+
+    public Student(int idNum, int GPA) {
+        this.idNum = idNum;
+        this.GPA = GPA;
     }
 
     @Override
     public String toString() {
-        return "Student's id: " +
-                 id;
+        return "Student{" +
+                "idNum=" + idNum +
+                ", GPA=" + GPA +
+                '}';
+    }
+
+    public int getIdNum() {
+        return idNum;
+    }
+
+    public void setIdNum(int idNum) {
+        this.idNum = idNum;
+    }
+
+    public int getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(int GPA) {
+        this.GPA = GPA;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(!(o instanceof Student))
+            throw new IllegalArgumentException("Объект не является студентом!");
+        // < 0 -> o, 0 -> ==, >0 -> this;
+        return this.idNum - ((Student) o).idNum;
     }
 }
